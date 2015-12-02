bc3ddc35b52cb34f0c43db9fd7927bf5207997f7
Mave <info@wesleybuurke.nl>
2015-12-02 23:47:25 +0100

    Added second half: ribbon needed

--- day2.php
+++ day2.php
@@ -1,10 +1,11 @@
 <?php
 
 class Present {
-	public $l, $w, $h, $paper_needed = 0;
-
-	public function __construct() {
-	}
+	public 	$l = 0,
+			$w = 0,
+			$h = 0,
+			$paper_needed = 0,
+			$ribbon_needed = 0;
 
 	public function wrappingNeeded() {
 		$lw = $this->l * $this->w;
@@ -17,10 +18,19 @@ class Present {
 		$this->paper_needed += min([$lw, $lh, $wh]);
 	}
 
+	public function ribbonNeeded() {
+		$sizes = [$this->l, $this->w, $this->h];
+		sort($sizes, SORT_NUMERIC);
+		array_pop($sizes);
+
+
+		$this->ribbon_needed += $sizes[0] + $sizes[0] + $sizes[1] + $sizes[1];
+		$this->ribbon_needed += $this->l * $this->w * $this->h;
+	}
 }
 
 
-$total_paper_needed = 0;
+$total_paper_needed = $total_ribbon_needed = 0;
 
 $file = file_get_contents("input/day2.txt");
 $lines = explode("\n", $file);
@@ -37,8 +47,12 @@ foreach($lines as $line) {
 	$present->h = $h;
 
 	$present->wrappingNeeded();
+	$present->ribbonNeeded();
 
 	$total_paper_needed += $present->paper_needed;
+	$total_ribbon_needed += $present->ribbon_needed;
+
 }
 
-echo $total_paper_needed;
+echo 'Total amount of paper needed: ' . $total_paper_needed . '<br>';
+echo 'Total amount of ribbon needed: ' . $total_ribbon_needed . '<br>';
