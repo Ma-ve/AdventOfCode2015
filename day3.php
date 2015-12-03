13010c69dce43bf1bc32809159137c82816a97ff
Mave <info@wesleybuurke.nl>
2015-12-03 20:36:12 +0100

    First half + second half of Day 3

--- day3.php
+++ day3.php
@@ -1,5 +1,17 @@
 <?php
 
+//Compare function for array sorting
+function cmp($a, $b) {
+    if ($a == $b) {
+        return 0;
+    }
+    return ($a < $b) ? 1 : -1;
+}
+
+/**
+First half
+*/
+echo '<h1>Day 3 - First half</h1>';
 $directions = [
 	'^' => 0,
 	'>' => 0,
@@ -22,16 +34,50 @@ for($i = 0; $i < strlen($file); $i++) {
 	$visited_houses[$coords] = isset($visited_houses[$coords]) ? $visited_houses[$coords] + 1 : 1;
 }
 
-function cmp($a, $b) {
-    if ($a == $b) {
-        return 0;
-    }
-    return ($a < $b) ? 1 : -1;
+
+echo 'Unique houses visited: ' . count($visited_houses) . '<br>';
+echo 'Top 30 houses visited, by most visited:<br>';
+uasort($visited_houses, 'cmp');
+echo '<pre>';
+print_r(array_slice($visited_houses, 0, 30));
+echo '</pre>';
+
+/**
+Second half
+*/
+
+echo '<h1>Day 3 - Second half</h1>';
+$directions['santa'] = $directions['robo_santa'] = [
+	'^' => 0,
+	'>' => 0,
+];
+
+$visited_houses = ['0,0' => 1];
+
+$file = file_get_contents("input/day3.txt");
+
+for($i = 0; $i < strlen($file); $i++) {
+	switch($i % 2) {
+		case 0: $person = 'santa'; break;
+		case 1: $person = 'robo_santa'; break;
+	}
+
+	$content = $file[$i];
+	switch($content) {
+		case "^": $directions[$person]['^']++; break;
+		case ">": $directions[$person]['>']++; break;
+		case "v": $directions[$person]['^']--; break;
+		case "<": $directions[$person]['>']--; break;
+	}
+
+	$coords = implode(',', $directions[$person]);
+	$visited_houses[$coords] = isset($visited_houses[$coords]) ? $visited_houses[$coords] + 1 : 1;
 }
 
 
 echo 'Unique houses visited: ' . count($visited_houses) . '<br>';
-echo 'Houses visited, by most visited:<br>';
+echo 'Top 30 houses visited, by most visited:<br>';
 uasort($visited_houses, 'cmp');
 echo '<pre>';
-print_r($visited_houses);
+print_r(array_slice($visited_houses, 0, 30));
+echo '</pre>';
