0f4c6d776513f249bf39031625abe6c70ee36f92
Mave <info@wesleybuurke.nl>
2015-12-04 20:04:38 +0100

    Day 4 - First half

---
+++ day4.php
@@ -0,0 +1,24 @@
+<?php
+
+$input = file_get_contents("input/day4.txt");
+
+function find_leading_zeroes($input, $zeroes = 5) {
+	$match_zeroes = "";
+	for($i = 0; $i < $zeroes; $i++) {
+		$match_zeroes .= "0";
+	}
+
+	$i = 0;
+	while(true) {
+		$i++;
+		if(strpos(md5($input . $i), $match_zeroes) === 0) {
+			return $i;
+		}
+	}
+}
+
+/**
+First half
+*/
+echo '<h1>Day 4 - First half</h1>';
+echo 'Starting with 5 zeroes: ' . find_leading_zeroes($input);
