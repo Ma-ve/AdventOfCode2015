f5f9a56ed3ece6c7f2ca9644a973cecc275f7aba
Mave <info@wesleybuurke.nl>
2015-12-04 21:42:32 +0100

    Also show time of finding hash

--- day4.php
+++ day4.php
@@ -3,6 +3,7 @@
 $input = file_get_contents("input/day4.txt");
 
 function find_leading_zeroes($input, $zeroes = 5) {
+	$time = microtime(true);
 	$match_zeroes = "";
 	for($i = 0; $i < $zeroes; $i++) {
 		$match_zeroes .= "0";
@@ -12,7 +13,7 @@ function find_leading_zeroes($input, $zeroes = 5) {
 	while(true) {
 		$i++;
 		if(strpos(md5($input . $i), $match_zeroes) === 0) {
-			return $i;
+			return ['time' => round(microtime(true) - $time, 6), 'i' => $i];
 		}
 	}
 }
@@ -21,10 +22,12 @@ function find_leading_zeroes($input, $zeroes = 5) {
 First half
 */
 echo '<h1>Day 4 - First half</h1>';
-echo 'Starting with 5 zeroes: ' . find_leading_zeroes($input);
+$output = find_leading_zeroes($input);
+echo 'Starting with 5 zeroes: ' . $output['i'] . ' (' . $output['time'] . 's)';
 
 /**
 Second half
 */
 echo '<h1>Day 4 - Second half</h1>';
-echo 'Starting with 6 zeroes: ' . find_leading_zeroes($input, 6);
+$output = find_leading_zeroes($input, 6);
+echo 'Starting with 6 zeroes: ' . $output['i'] . ' (' . $output['time'] . 's)';
