c171b47f44cfddc9eab04542f2084e3c5263400c
Mave <info@wesleybuurke.nl>
2015-12-06 11:52:31 +0100

    Both parts

--- day6.php
+++ day6.php
@@ -1,11 +1,24 @@
 <?php
 
-//Copy this file, rename it to day{int}.php
+// ini_set('memory_limit', '256M');
+
+if(!isset($_GET['part'])) {
+	echo '<h1><a href="?part=1">Part 1</a></h1>';
+	echo '<h1><a href="?part=2">Part 2</a></h1>';
+	echo '<h1><a href="?part=both">Both (memory intensive)</a></h1>';
+} else {
+	echo '<h6><a href="?">Return</a></h6>';
+}
+
+$_GET['part'] = isset($_GET['part']) ? $_GET['part'] : false;
+
+if($_GET['part'] == 'both') {
+	ini_set('memory_limit', '256M');
+}
 
 $path = explode("\\", __FILE__);
 $file = array_pop($path);
 $day = substr(explode(".", $file)[0], 3);
-
 
 $input = file_get_contents("input/day" . $day . ".txt");
 
@@ -36,129 +49,136 @@ function reset_grid($size, $reset_value = false) {
 	return $grid;
 }
 
-/**
-First half
-*/
-echo '<h1>Day ' . $day . ' - First half</h1>';
 
-//Business logic
+if($_GET['part'] == 1 || $_GET['part'] == 'both') {
 
-$time = microtime(true);
+	/**
+	First half
+	*/
+	echo '<h1>Day ' . $day . ' - First half</h1>';
 
-/**
-@param string $type - (on|off|toggle)
-@param array $start - Start positions ([0] = x, [1] = y)
-@param array $end - End positions ([0] = x, [1] = y)
-*/
-function modify_grid_part_1($type, $start, $end) {
-	global $grid, $matches;
+	//Business logic
 
-	for($i = $start[0]; $i <= $end[0]; $i++) {
-		for($j = $start[1]; $j <= $end[1]; $j++) {
-			$grid[$i][$j] = ($type == 'on' ? true : ($type == 'off' ? false : !$grid[$i][$j]));
-		}
-	}
-}
+	$time = microtime(true);
 
-/**
-@param array $grid - Grid of 1000*1000 lights
-*/
-function return_active($grid) {
-	$active = 0;
-	foreach($grid as $line) {
-		foreach($line as $column) {
-			if($column) {
-				$active++;
+	/**
+	@param string $type - (on|off|toggle)
+	@param array $start - Start positions ([0] = x, [1] = y)
+	@param array $end - End positions ([0] = x, [1] = y)
+	*/
+	function modify_grid_part_1($type, $start, $end) {
+		global $grid, $matches;
+
+		for($i = $start[0]; $i <= $end[0]; $i++) {
+			for($j = $start[1]; $j <= $end[1]; $j++) {
+				$grid[$i][$j] = ($type == 'on' ? true : ($type == 'off' ? false : !$grid[$i][$j]));
 			}
 		}
 	}
 
-	return $active;
-}
-
-$grid = reset_grid(1000);
-
-$i = 0;
-foreach(explode("\n", $input) as $line) {
-	foreach($possibilities as $p) {
-		if(strpos($line, $p) !== false) {
-			$start = explode(',', $matches[2 * $i]);
-			$end = explode(',', $matches[(2 * $i) + 1]);
-			modify_grid_part_1($p, $start, $end);
+	/**
+	@param array $grid - Grid of 1000*1000 lights
+	*/
+	function return_active($grid) {
+		$active = 0;
+		foreach($grid as $line) {
+			foreach($line as $column) {
+				if($column) {
+					$active++;
+				}
+			}
 		}
+
+		return $active;
 	}
-	$i++;
+
+	$grid = reset_grid(1000);
+
+	$i = 0;
+	foreach(explode("\n", $input) as $line) {
+		foreach($possibilities as $p) {
+			if(strpos($line, $p) !== false) {
+				$start = explode(',', $matches[2 * $i]);
+				$end = explode(',', $matches[(2 * $i) + 1]);
+				modify_grid_part_1($p, $start, $end);
+			}
+		}
+		$i++;
+	}
+
+	echo 'Lights that are on: ' . return_active($grid) . '<br>';
+	echo 'Time: ' . round(microtime(true) - $time, 6) . 's';
 }
 
-echo 'Lights that are on: ' . return_active($grid) . '<br>';
-echo 'Time: ' . round(microtime(true) - $time, 6) . 's';
 
 
 
+if($_GET['part'] == 2 || $_GET['part'] == 'both') {
+	/**
+	Second half
+	*/
+	echo '<h1>Day ' . $day . ' - Second half</h1>';
 
+	//Business logic
 
-/**
-Second half
-*/
-echo '<h1>Day ' . $day . ' - Second half</h1>';
+	$time = microtime(true);
 
-//Business logic
+	/**
+	@param string $type - (on|off|toggle)
+	@param array $start - Start positions ([0] = x, [1] = y)
+	@param array $end - End positions ([0] = x, [1] = y)
+	*/
+	function modify_grid_part_2($type, $start, $end) {
+		global $grid, $matches;
 
-
-/**
-@param string $type - (on|off|toggle)
-@param array $start - Start positions ([0] = x, [1] = y)
-@param array $end - End positions ([0] = x, [1] = y)
-*/
-function modify_grid_part_2($type, $start, $end) {
-	global $grid, $matches;
-
-	for($i = $start[0]; $i <= $end[0]; $i++) {
-		for($j = $start[1]; $j <= $end[1]; $j++) {
-			switch($type) {
-				case 'on':
-					$grid[$i][$j]++;
-					break;
-				case 'off':
-					$grid[$i][$j] = --$grid[$i][$j] < 0 ? 0 : $grid[$i][$j];
-					break;
-				case 'toggle':
-					$grid[$i][$j] += 2;
-					break;
+		for($i = $start[0]; $i <= $end[0]; $i++) {
+			for($j = $start[1]; $j <= $end[1]; $j++) {
+				switch($type) {
+					case 'on':
+						$grid[$i][$j]++;
+						break;
+					case 'off':
+						$grid[$i][$j] = --$grid[$i][$j] < 0 ? 0 : $grid[$i][$j];
+						break;
+					case 'toggle':
+						$grid[$i][$j] += 2;
+						break;
+				}
 			}
 		}
 	}
-}
 
-/**
-@param array $grid - Grid of 1000*1000 lights
-*/
-function return_brightness($grid) {
-	$brightness = 0;
-	foreach($grid as $line) {
-		foreach($line as $column) {
-			if($column) {
-				$brightness += $column;
+	/**
+	@param array $grid - Grid of 1000*1000 lights
+	*/
+	function return_brightness($grid) {
+		$brightness = 0;
+		foreach($grid as $line) {
+			foreach($line as $column) {
+				if($column) {
+					$brightness += $column;
+				}
 			}
 		}
+
+		return $brightness;
 	}
 
-	return $brightness;
-}
+	$grid = reset_grid(1000, 0); //Brightness mode
 
-$grid = reset_grid(1000, 0); //Brightness mode
+	$i = 0;
 
-$i = 0;
-foreach(explode("\n", $input) as $line) {
-	foreach($possibilities as $p) {
-		if(strpos($line, $p) !== false) {
-			$start = explode(',', $matches[2 * $i]);
-			$end = explode(',', $matches[(2 * $i) + 1]);
-			modify_grid_part_2($p, $start, $end);
+	foreach(explode("\n", $input) as $line) {
+		foreach($possibilities as $p) {
+			if(strpos($line, $p) !== false) {
+				$start = explode(',', $matches[2 * $i]);
+				$end = explode(',', $matches[(2 * $i) + 1]);
+				modify_grid_part_2($p, $start, $end);
+			}
 		}
+		$i++;
 	}
-	$i++;
-}
 
-echo 'Total brightness of all lights: ' . return_brightness($grid) . '<br>';
-echo 'Time: ' . round(microtime(true) - $time, 6) . 's';
+	echo 'Total brightness of all lights: ' . return_brightness($grid) . '<br>';
+	echo 'Time: ' . round(microtime(true) - $time, 6) . 's';
+}
